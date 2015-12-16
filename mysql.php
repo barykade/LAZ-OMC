233f7e95ecdab23ad882e89958df262e274b7505
root <root@sam-Server>
2015-12-16 14:34:43 -0500

    updated mysql to sign in as root with password

--- mysql.php
+++ mysql.php
@@ -2,7 +2,7 @@
 
 $mysql_host = "localhost";
 $mysql_user = "root";
-$mysql_pass = "";
+$mysql_pass = "andersonjake";
 @mysql_connect($mysql_host, $mysql_user, $mysql_pass) or die('could not connect!');
 
 $db = "laz_omc";
